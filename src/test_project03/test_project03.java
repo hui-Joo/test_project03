cff1f9b5f7ca9b1b91ecef23f9394aacc510a948
Choi.SY <sollly0829@gmail.com>
2021-08-12 15:02:03 +0900

    경유 주유기능 추가

--- src/test_project03/test_project03.java
+++ src/test_project03/test_project03.java
@@ -2,15 +2,19 @@ package test_project03;
 
 import java.util.Scanner;
 
+import test_project03_soyoung.Diesel;
+
 public class test_project03 {
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		Diesel disel = new Diesel();
 		int num;
 		System.out.println("===웰컴 주유소===");
 		while(true) {
 			System.out.println("1.휘발유");
 			System.out.println("2.경유");
+			disel.putDiesel();
 			System.out.println("3.등유");
 			System.out.println("4.계산");
 			System.out.println("5.세차");
---
+++ src/test_project03_soyoung/Diesel.java
@@ -0,0 +1,8 @@
+package test_project03_soyoung;
+
+public class Diesel {
+
+	public void putDiesel() {
+		System.out.println("경유 주유합니다");
+	}
+}
