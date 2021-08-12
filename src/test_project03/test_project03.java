3e47804f8d00f73bcadd0165acd44d9af9599a91
hjoo <oct20ray@gmail.com>
2021-08-12 14:17:37 +0900

    03 프로젝트 생성

---
+++ src/test_project03/test_project03.java
@@ -0,0 +1,24 @@
+package test_project03;
+
+import java.util.Scanner;
+
+public class test_project03 {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+		System.out.println("===웰컴 주유소===");
+		while(true) {
+			System.out.println("1.휘발유");
+			System.out.println("2.경유");
+			System.out.println("3.등유");
+			System.out.println("4.계산");
+			System.out.println("5.세차");
+			System.out.println(">>> : ");
+			num = input.nextInt();
+			
+			switch(num) {
+			}
+		}
+	}
+}
